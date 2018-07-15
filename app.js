2a8488037d9d4aaa1f8ae3092e74dc951cafd3f1
Max Ivanov <mak.ivanov@gmail.com>
2018-07-15 21:16:14 +0300

    Add project files

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const app = express();
+
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/cards', (req, res) => {
+  res.render('card', { 
+    prompt: "Who is buried in Grant's tomb?",
+    hint: "Think about whose tomb it is."
+  });
+});
+
+app.get('/hello', (req, res) => {
+  res.render('hello');
+});
+
+app.listen(3000, () => {
+  console.log('The app is running on localhost:3000!')
+});
