17136e8a7f0eeca4e7cf0c3e3d56361988bca614
Max Ivanov <mak.ivanov@gmail.com>
2018-07-16 09:16:58 +0300

    added a new view and redirect logic

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-  res.render('index');
+  const name = req.cookies.username;
+  if (name) {
+    res.render('index', {name});
+  } else {
+    res.redirect('/hello');
+  }
 });
 
 app.get('/cards', (req, res) => {
@@ -19,12 +26,23 @@ app.get('/cards', (req, res) => {
 });
 
 app.get('/hello', (req, res) => {
-  res.render('hello');
+  const name = req.cookies.username;
+  if (name) {
+    res.redirect('/')
+  } else {
+    res.render('hello');
+  }
 });
 
 app.post('/hello', (req, res) => {
-  res.render('hello', {name: req.body.username});
+  res.cookie('username', req.body.username);
+  res.redirect('/');
 });
+
+app.post('/goodbye', (req, res) => {
+  res.clearCookie('username');
+  res.redirect('/hello');
+})
 
 app.listen(3000, () => {
   console.log('The app is running on localhost:3000!')
